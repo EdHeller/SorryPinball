4420bff080289bb8c1d76804f448dd4859ce6c3d
Ed Heller <edstuff@live.com>
2020-11-09 01:29:58 +0000

    Initial commit

---
+++ conf.lua
@@ -0,0 +1,7 @@
+-- LÖVE configuration file
+
+love.conf = function(t)
+  --t.console = true
+  t.window.width = 400
+  t.window.height = 240
+end
---
+++ main.lua
@@ -0,0 +1,16 @@
+local anim8 = require 'anim8'
+local gfx = love.graphics
+local timer = love.timer
+local maths = love.math
+local window = love.window
+
+local soundVolume = 100
+
+function love.load()
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+end
