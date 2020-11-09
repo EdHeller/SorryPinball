74e50419baa0a973eeaf35e5b869989e8f3bddd3
Ed Heller <edstuff@live.com>
2020-11-09 01:40:41 +0000

    started the project

--- main.lua
+++ main.lua
@@ -5,11 +5,17 @@ local maths = love.math
 local window = love.window
 
 local soundVolume = 100
+local ballx = 40
+local bally = 40
 
 function love.load()
 end
 
 function love.draw()
+love.graphics.circle( 'line', ballx, bally, 20)
+
+--resting line 28 long 3 tall
+
 end
 
 function love.update(dt)
