28d3b98e4c18022454b37339f973a291694071a0
Ed Heller <edstuff@live.com>
2020-11-09 02:16:07 +0000

    changed line to rectangle now to just draw the rest of the board

--- main.lua
+++ main.lua
@@ -5,15 +5,23 @@ local maths = love.math
 local window = love.window
 
 local soundVolume = 100
-local ballx = 40
-local bally = 40
+local ballx = 353
+local bally = 164
+
+--board size 520
+--view size 240...
+
+--90px right slant
+--plunger 15 x 30
 
 function love.load()
 end
 
 function love.draw()
-love.graphics.circle( 'line', ballx, bally, 20)
+love.graphics.circle( 'fill', ballx, bally, 7)
 
+love.graphics.rectangle( 'fill', 347, 170, 13, 3)
+--ball is 13x13
 --resting line 28 long 3 tall
 
 end
